5b456f1cf9e61153398ce790577d258439f0b634
S543471 <91288086+S543471@users.noreply.github.com>
2023-02-21 11:58:25 -0600

    While loop created, scanner created, file opened

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -6,6 +6,8 @@
 package project01;
 
 import java.util.*;
+import java.io.*;
+import javax.swing.*;
 
 //
 public class Project01 {
@@ -13,8 +15,31 @@ public class Project01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String choice;
+        //Scanner
+        Scanner object = new Scanner(System.in);
+
+        //Opening and reading File
+        File inputFile = new File("netflix.txt");
+        Scanner input = new Scanner(inputFile);
+
+        //User prompt while loop
+        System.out.println("What would you like to do");
+        System.out.println("Menu (Choose the following or Q to quit):");
+        System.out.println("F -Filter on Type");
+        System.out.println("D -Filter on Date");
+        System.out.println("S -Search by show");
+        System.out.println("H -Highest Days in Top Ten");
+        System.out.print("Choice: ");
+        choice = object.next();
+        while (choice != "q") {
+
+            System.out.println();
+            System.out.print("Choice: ");
+            choice = object.next();
+        }
 
     }
 
