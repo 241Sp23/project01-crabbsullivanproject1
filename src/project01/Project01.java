35e9bc043ffb107e3cbec2d791db220b5e886c5d
kaicrabb <124805211+kaicrabb@users.noreply.github.com>
2023-02-28 19:30:16 -0600

    while loop works, prints to file for show method, user can type in lower or upper case for methods, still won't print to date.txt or type.txt

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -15,8 +15,9 @@ public class Project01 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         //Setting user input variable
         String choice;
         //Scanner
@@ -27,47 +28,10 @@ public class Project01 {
         myChooser.showOpenDialog(null);
         File netflixFile = myChooser.getSelectedFile();
 
-        //File Scanner
-        Scanner file = new Scanner(netflixFile);
-
         //User prompt while loop
-        System.out.println("What would you like to do");
-        System.out.println("Menu (Choose the following or Q to quit):");
-        System.out.println("F -Filter on Type");
-        System.out.println("D -Filter on Date");
-        System.out.println("S -Search by show");
-        System.out.println("H -Highest Days in Top Ten");
-        System.out.print("Choice: ");
-        choice = object.next();
-
-        //While loop
-        while (!"q".equals(choice)) {
-
-            //Puts data on array
-            String[][] netflix = find(file);
-            //Filters type of show/movie
-            if ("f".equals(choice)) {
-                System.out.print("Would you like to filter on TV Show, Movie, or Stand-Up Comedy: ");
-                String typeChoice = object.next();
-                filterOnType(netflix, typeChoice);
-            } //Searches for everything on the given date
-            else if ("d".equals(choice)) {
-                System.out.print("What week would you like to search for (mm/dd/yyyy): ");
-                String dateChoice = object.next();
-                filterOnDate(netflix, dateChoice);
-            } //Search for show
-            else if ("s".equals(choice)) {
-                System.out.print("What TV Show or Movie would you like to search for: ");
-                String mediaChoice = object.next();
-                searchForShow(netflix, mediaChoice);
-            } //Gives top 10
-            else if ("h".equals(choice)) {
-                highestDaysInTopTen(netflix);
-            } else {
-                System.out.println("Invalid Input");
-            }
-
-            System.out.println();
+        try ( //File Scanner
+                Scanner file = new Scanner(netflixFile)) {
+            //User prompt while loop
             System.out.println("What would you like to do");
             System.out.println("Menu (Choose the following or Q to quit):");
             System.out.println("F -Filter on Type");
@@ -76,59 +40,103 @@ public class Project01 {
             System.out.println("H -Highest Days in Top Ten");
             System.out.print("Choice: ");
             choice = object.next();
+            //While loop
+            String[][] netflix = find(file);
+            
+            while (!"q".equals(choice)) {
+                //Filters type of show/movie
+                if ("f".equals(choice)) {
+                    System.out.print("Would you like to filter on TV Show, Movie, or Stand-Up Comedy: ");
+                    String typeChoice = object.next();
+                    filterOnType(netflix, typeChoice);
+                } //Searches for everything on the given date
+                else if ("d".equals(choice)) {
+                    System.out.print("What week would you like to search for (mm/dd/yyyy): ");
+                    String dateChoice = object.next();
+                    filterOnDate(netflix, dateChoice);
+                } //Search for show
+                else if ("s".equals(choice)) {
+                    System.out.print("What TV Show or Movie would you like to search for: ");
+                    String mediaChoice = object.next();
+                    searchForShow(netflix, mediaChoice);
+                } //Gives top 10
+                else if ("h".equals(choice)) {
+                    highestDaysInTopTen(netflix);
+                }
+                else {
+                    System.out.println("Invalid Input");
+                }
+                
+                System.out.println();
+                System.out.println("What would you like to do");
+                System.out.println("Menu (Choose the following or Q to quit):");
+                System.out.println("F -Filter on Type");
+                System.out.println("D -Filter on Date");
+                System.out.println("S -Search by show");
+                System.out.println("H -Highest Days in Top Ten");
+                System.out.print("Choice: ");
+                choice = object.next();
+            }
+            //Closing File
         }
-        //Closing File
-        file.close();
 
     }
 
     public static String[][] find(Scanner fileInput) {
         String[][] netflixArray = new String[7101][6];
-        while (fileInput.hasNextLine()) {
             for (int i = 0; i < netflixArray.length; i++) {
-                String record = fileInput.nextLine();
-                String[] line = record.split(",");
-                netflixArray[i] = line;
+                String[] Array2 = fileInput.nextLine().strip().split(",");
+                System.arraycopy(Array2, 0, netflixArray[i], 0, 6);
+                
             }
-        }
         return netflixArray;
     }
 
-    public static void searchForShow(String[][] dataSet, String show) throws FileNotFoundException {
+    public static void searchForShow(String[][] dataSet, String show) throws FileNotFoundException, IOException {
         int count = 0;
-        PrintWriter showFile = new PrintWriter("searchResults.txt");
-        for (int i = 1; i < dataSet.length; i++) {
-            if (dataSet[i][2] == show) {
-                count++;
-                showFile.printf("%-12s%-7s%-36s%-12s%-13s%-5s\n", dataSet[i][0], dataSet[i][1], dataSet[i][2], dataSet[i][3], dataSet[i][4], dataSet[i][5]);
+        try (FileWriter showFile = new FileWriter("searchResults.txt")) {
+            for (int i = 1; i < dataSet.length; i++) {
+                String dataLower = (dataSet[i][2]).toLowerCase();
+                String showLower = show.toLowerCase();
+                if (dataLower.equals(showLower)) {
+                    for (int x = 0; x < dataSet[i].length; x++) {
+                        showFile.write(dataSet[i][x] + " ");
+                        count ++;
+                    }
+                }
+            }
+            if (count > 0) {
+                System.out.println("The number of weeks " + show + " appeared in: " + count/6);
+            }
+            else {
+                System.out.println("Show not found");
             }
         }
-        if (count > 0) {
-            System.out.println("The number of weeks " + show + "appeared in: " + count);
-        } else {
-            System.out.println("Show not found");
+    }
+
+    public static void filterOnType(String[][] dataSet, String type) throws FileNotFoundException, IOException {
+        try (FileWriter typeFile = new FileWriter("type.txt")) {
+            for (int i = 1; i < dataSet.length; i++) {
+                String dataLower = (dataSet[i][2]).toLowerCase();
+                String typeLower = type.toLowerCase();
+                if (dataLower.equals(typeLower))
+                    for (int x = 0; i < dataSet[i].length; x++) {
+                        typeFile.write(dataSet[i][x] + " ");
+                    }
+                }
+        }
+            }
+
+    public static void filterOnDate(String[][] dataSet, String date) throws FileNotFoundException, IOException {
+        FileWriter showFile = new FileWriter("date.txt");
+        for (int i = 1; i < dataSet.length; i++) {
+            if (dataSet[i][0].equals(date)) {
+                for (int x = 0; i < dataSet[i].length; x++){
+                    showFile.write(dataSet[i][x] + " ");
+                }
         }
         showFile.close();
     }
-
-    public static void filterOnType(String[][] dataSet, String type) throws FileNotFoundException {
-        PrintWriter typeFile = new PrintWriter("type.txt");
-        for (int i = 1; i < dataSet.length; i++) {
-            if (dataSet[i][3] == type) {
-                typeFile.printf("%-12s%-7s%-36s%-12s%-13s%-5s\n", dataSet[i][0], dataSet[i][1], dataSet[i][2], dataSet[i][3], dataSet[i][4], dataSet[i][5]);
-            }
-        }
-        typeFile.close();
-    }
-
-    public static void filterOnDate(String[][] dataSet, String date) throws FileNotFoundException {
-        PrintWriter showFile = new PrintWriter("date.txt");
-        for (int i = 1; i < dataSet.length; i++) {
-            if (dataSet[i][0] == date) {
-                showFile.printf("%-12s%-7s%-36s%-12s%-13s%-5s\n", dataSet[i][0], dataSet[i][1], dataSet[i][2], dataSet[i][3], dataSet[i][4], dataSet[i][5]);
-            }
-        }
-        showFile.close();
     }
 
     public static void highestDaysInTopTen(String[][] dataSet) {
